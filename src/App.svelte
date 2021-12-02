a26b820b77e5b2ce04e22c019856088b3eba9d4c
zwht81 <66426651+zwht81@users.noreply.github.com>
2021-12-02 15:51:05 +0800

    绑定账号密码

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,16 @@
 <script>
 	export let name;
+
+	let username='';
+	let password='';
+
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<input bind:value={username}>
+	<input type="password" bind:value={password}>
+	<p>Hello {username} {password}!</p>
 </main>
 
 <style>
